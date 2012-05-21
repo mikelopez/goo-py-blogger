8fe228cc631f1c4fd6e59739c4619d4727419073
Marcos Lopez <dev@scidentify.info>
2012-05-20 22:08:59 -0400

    tabindenting

--- googleBlogger.py
+++ googleBlogger.py
@@ -5,83 +5,81 @@ import atom
 # old script from 2009 
 
 class GoogleBlogger:
-	""" a basic google API interface / blogger 
-	Author: (c) Marcos Lopez - dev@scidentify.info """
+  """ a basic google API interface / blogger 
+  Author: (c) Marcos Lopez - dev@scidentify.info """
 
-	blogger = ''
-	user = ''
-	password = ''
-	next = 'https://next-url/'
-	scope = 'http://www.blogger.com/feeds'
-	source = 'app-source-name-1.0'
-	service = 'blogger'
+  blogger = ''
+  user = ''
+  password = ''
+  next = 'https://next-url/'
+  scope = 'http://www.blogger.com/feeds'
+  source = 'app-source-name-1.0'
+  service = 'blogger'
+  server = 'www.blogger.com'
+  account_type = 'GOOGLE'
 
-	def __init__(self, username=None, password=None):
-		""" login if passed username arg / password is optional"""
-		if username:
-			self.user = username
-			if password:
-				# use same pw or use arg password
-				self.password = password
-			self.BloggerLogin()
+  def __init__(self, username=None, password=None):
+    """ login if passed username arg / password is optional"""
+    if username:
+      self.user = username
+      if password:
+        # use same pw or use arg password
+        self.password = password
+      self.BloggerLogin()
 
-	def BloggerLogin(self):
-		""" authenticate to blogger api """
-		try:
-			secure = False
-			session = True
-			self.blogger = service.GDataService(self.user, self.password)
-			self.blogger.source = self.source
-			self.blogger.service = self.service
-			self.blogger.account_type = 'GOOGLE'
-			self.blogger.server = 'www.blogger.com'
-			self.blogger.ProgrammaticLogin()
-			return True
-		except service.BadAuthentication:
-			return False
+  def BloggerLogin(self):
+    """ authenticate to blogger api """
+    try:
+      secure = False
+      session = True
+      self.blogger = service.GDataService(self.user, self.password)
+      self.blogger.source = self.source
+      self.blogger.service = self.service
+      self.blogger.server = self.server
+      self.blogger.account_type = self.account_type
+      self.blogger.ProgrammaticLogin()
+      return True
+    except service.BadAuthentication:
+      return False
 
-	def BloggerGetBlogs(self):
-		""" return list of blogs """
-		query = service.Query()
-		query.feed = '/feeds/default/blogs'
-		feed = self.blogger.Get(query.ToUri())
-		return feed
+  def BloggerGetBlogs(self):
+    """ return list of blogs """
+    query = service.Query()
+    query.feed = '/feeds/default/blogs'
+    feed = self.blogger.Get(query.ToUri())
+    return feed
 
-	def BloggerGetPosts(self, blogid):
-		""" get blog posts for blogid """
-		feed = self.blogger.GetFeed('/feeds/' + blogid + '/posts/default')
+  def BloggerGetPosts(self, blogid):
+    """ get blog posts for blogid """
+    feed = self.blogger.GetFeed('/feeds/' + blogid + '/posts/default')
 
-		return feed
+    return feed
 
-	def BloggerPostBlog(self, blogid, title, content):
-		""" post a blog as html content """
-		entry = gdata.GDataEntry()
-		entry.title = atom.Title('xhtml', title)
-		entry.content = atom.Content(content_type='html', text=content)
-		return self.blogger.Post(entry, '/feeds/%s/posts/default' % (blogid))
+  def BloggerPostBlog(self, blogid, title, content):
+    """ post a blog as html content """
+    entry = gdata.GDataEntry()
+    entry.title = atom.Title('xhtml', title)
+    entry.content = atom.Content(content_type='html', text=content)
+    return self.blogger.Post(entry, '/feeds/%s/posts/default' % (blogid))
 
 
 
 
 # main
 if __name__ == '__main__':
-	# init blogger class and login
-	blog = googleBlogger()
-	blog.BloggerLogin()
-	blogs = []
-	my_blogs = None
+  # init blogger class and login
+  blog = googleBlogger()
+  blog.BloggerLogin()
+  blogs = []
+  my_blogs = None
 
-	# retrieve list of blogs
-	my_blogs = blog.BloggerGetBlogs()
-	for feed in my_blogs.entry:
-		blogid = feed.GetSelfLink().href.split('/')[-1]
-		blogs.append({
-			'blogname':feed.title.text,
-			'blogid':blogid,
-		})
-
-	
-
-	
+  # retrieve list of blogs
+  my_blogs = blog.BloggerGetBlogs()
+  for feed in my_blogs.entry:
+    blogid = feed.GetSelfLink().href.split('/')[-1]
+    blogs.append({
+      'blogname':feed.title.text,
+      'blogid':blogid,
+    })
 
 
--- tests.py
+++ tests.py
@@ -8,48 +8,47 @@ this test does not go through posting to your blog.
 
 class TestBlogger(unittest.TestCase):
 
-	blogger = None
-	username = ""
-	password = ""
+  blogger = None
+  username = ""
+  password = ""
 
-	def test_basic(self):
-		""" test basic init of this class """
-		sys.path.append('.')
-		log = logging.getLogger("TestBlogger.test_basic")
+  def test_basic(self):
+    """ test basic init of this class """
+    sys.path.append('.')
+    log = logging.getLogger("TestBlogger.test_basic")
 
-		from googleBlogger import *
-		self.blogger = GoogleBlogger()
-		self.assertTrue(self.blogger)
-		self.blogger.user = self.username
-		self.blogger.password = self.password
+    from googleBlogger import *
+    self.blogger = GoogleBlogger()
+    self.assertTrue(self.blogger)
+    self.blogger.user = self.username
+    self.blogger.password = self.password
 
-		self.assertEquals(getattr(self.blogger, 'scope'), 'http://www.blogger.com/feeds')
-		self.assertEquals(getattr(self.blogger, 'service'), 'blogger')
-		
+    self.assertEquals(getattr(self.blogger, 'scope'), 'http://www.blogger.com/feeds')
+    self.assertEquals(getattr(self.blogger, 'service'), 'blogger')
 
-		login_result = self.blogger.BloggerLogin()
+    login_result = self.blogger.BloggerLogin()
 
-		if not login_result:
-			raise AssertionError("Login returned false, check authentication")
+    if not login_result:
+      raise AssertionError("Login returned false, check authentication")
 
-		log.info('Getting blogs')
-		b = self.blogger.BloggerGetBlogs()
+    log.info('Getting blogs')
+    b = self.blogger.BloggerGetBlogs()
 
-		if len(b.entry) < 1:
-			log.info('Less than 1 blog returned. Might be new account')
-		else:
-			log.info('Blogs found')
-			blogs = []
-			for feed in b.entry:
-				log.info('-- Blog: %s' % feed.title.text)
-				blogid = feed.GetSelfLink().href.split('/')[-1]
-				blogs.append({
-					'blogname': feed.title.text, 'blogid': blogid,
-				})
+    if len(b.entry) < 1:
+      log.info('Less than 1 blog returned. Might be new account')
+    else:
+      log.info('Blogs found')
+      blogs = []
+      for feed in b.entry:
+        log.info('-- Blog: %s' % feed.title.text)
+        blogid = feed.GetSelfLink().href.split('/')[-1]
+        blogs.append({
+          'blogname': feed.title.text, 'blogid': blogid,
+        })
 
 
 if __name__ == '__main__':
-	logging.basicConfig(stream=sys.stdout)
-	logging.getLogger("TestBlogger.test_basic").setLevel(logging.INFO)
+  logging.basicConfig(stream=sys.stdout)
+  logging.getLogger("TestBlogger.test_basic").setLevel(logging.INFO)
 
-	unittest.main()
+  unittest.main()
